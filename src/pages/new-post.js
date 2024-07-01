929b54225bcef1286c45ba5cabb80150041ec9cb
authcornelius <corneliusedos@gmail.com>
2024-07-01 14:00:46 +0100

    some api consumed

--- src/pages/new-post.js
+++ src/pages/new-post.js
@@ -3,23 +3,13 @@ import '../styles/newPost.css';
 import { FaArrowLeft, FaUpload } from 'react-icons/fa6';
 import { Link } from 'react-router-dom';
 import Textarea from '../components/textArea';
+import GalleryLogo from '../assets/gallery.png'
 
 function NewPost() {
   const [textareaContent, setTextareaContent] = useState('');
-  const [isButtonDisabled, setIsButtonDisabled] = useState(true);
 
   const handleTextareaChange = (event) => {
-    const content = event.target.value;
-    setTextareaContent(content);
-    setIsButtonDisabled(content === ''); // Disable button if content is empty
-  };
-
-  const handlePublish = () => {
-    // Handle publish logic here
-    console.log("Publishing:", textareaContent);
-    // Reset textarea after publish if needed
-    setTextareaContent('');
-    setIsButtonDisabled(true); // Disable button after publish
+    setTextareaContent(event.target.value);
   };
 
   return (
@@ -35,24 +25,20 @@ function NewPost() {
 
           <div className='col-6 text-end publish d-flex justify-content-end'>
             <button className='btn btn-lg mx-5 one d-none d-lg-flex text-center'>Preview</button>
-            <button
-              className={`btn btn-lg two ${textareaContent ? 'btn-primary' : 'btn-secondary'}`}
-              onClick={handlePublish}
-              disabled={isButtonDisabled} // Disable button when no content
-            >
-              Publish
-            </button>
+            <button className={`btn btn-lg two ${textareaContent ? 'btn-primary' : 'btn-secondary'}`}>Publish</button>
           </div>
         </div>
       </div>
 
       <div className='container'>
-        <div className=' d-flex justify-content-center'>
-            <label htmlFor="image" className="custom-file-upload">
-                <FaUpload />
+        <div className='border mt-5 input d-flex justify-content-center align-items-center' style={{ height: '150px' }}>
+          <div className='text-center'>
+            <label htmlFor="image" className="custom-file-upload d-block col-12">
+              <img src={GalleryLogo} alt='Gallery' />
+              <p>Add Header Image</p>
             </label>
-            
-            <input type="file" placeholder='Add Header Image' id="image" name="image" accept="image/*" className='d-flex justify-content-center col-12'/>
+            <input type="file" id="image" name="image" accept="image/*" className='d-none'/>
+          </div>
         </div>
       </div>
 
